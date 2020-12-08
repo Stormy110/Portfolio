9e8a418dd3454aa80b17583a1ecc4680d62ef9c6
Ian Storms <IanStorms110@yahoo.com>
2020-12-08 17:35:51 -0500

    added pages

---
+++ index.js
@@ -0,0 +1,43 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const port = 3000;
+const hostname = '127.0.0.1';
+const server = http.createServer(app)
+const es6Renderer = require('express-es6-template-engine');
+app.engine('html', es6Renderer);
+app.set('views', 'templates');
+app.set('view engine', 'html');
+
+const projects = {
+    RecipeApp: 'I made a simple front-end recipe site that gets data from an API and displays it in a card.',
+    DribbbleDesign: 'I redesigned a site template found ond dribbble using CSS',
+    TamagatchiGame: 'I made a simple virtual pet game using Python.',
+    HangmanGame: 'I made a simple hangman game using python',
+    SageAdviceApp: "Team-made advice/joke/puppy picture app utilizing data from 3 API's"
+}
+
+app.get('/', (req,res)=>{
+    res.render('home')
+})
+
+app.get('/projects', (req,res)=>{
+    res.render('projects', {
+        locals: {
+            projects: Object.keys(projects)
+        }
+    })
+})
+app.get('/projects/:id', (req,res)=>{
+    const {id} = req.params;
+    res.render(`${id}`, {
+        locals: {
+            details: Object.values(projects),
+            projects: Object.keys(projects)
+        }
+    })
+})
+
+server.listen(port,hostname, ()=>{
+    console.log(`Running on http://${hostname}:${port}`)
+})
